84f8be4ff5b4faefe5404ee6d694780c29d60a36
Ujjwal Kumar <chaudharyujjwal302@gmail.com>
2021-05-16 08:54:45 +0530

    add auto filesystem

--- routes/scheme.js
+++ routes/scheme.js
@@ -1,14 +1,102 @@
 const express = require("express");
 const multer = require("multer");
+const fs = require("fs");
+const path = require("path");
 
 const multerStorage = multer.diskStorage({
   destination: (req, file, cb) => {
-    if(file.fieldname==="image"){
-        cb(null, "public/image");
+    
+      if(file.fieldname==="image"){
+        fs.access("public", function(error) {
+          if (error) {
+            console.log("Public Directory does not exist.")
+            fs.mkdir('./public/',(err)=>{
+              if(err) {
+                return console.log(err);
+              }
+              else{
+                console.log("Public Directory created.");
+                let dir= './public';
+                fs.mkdir(dir + '/image/', (err)=> {
+                  if (err){
+                    return console.error(err);
+                  } else{
+                    console.log("Image Directory created.");
+                    cb(null, "public/image");
+                  }
+                });
+              }
+            });
+          } 
+          else {
+            console.log("Public Directory exists.")
+            fs.access('public/image', function(error) {
+                  if(error) {
+                    console.log("Image Directory does not exist!!");
+                    fs.mkdir('./public/image',(err)=>{
+                      if(err) {
+                        return console.log(err);
+                      }
+                      else{
+                        console.log("Image directory created.");
+                        cb(null, "public/image");
+                      }
+                    });
+                  }
+                  else{
+                    console.log("Image Directory exists!!");
+                    cb(null, "public/image");
+                  }
+              });
+          }
+        });
+        
       }
-    else if(file.fieldname==="files"){
-        cb(null, "public/files");
-    }
+      else if(file.fieldname==="files"){
+        fs.access("public", function(error) {
+          if (error) {
+            console.log("Public Directory does not exist!!")
+            fs.mkdir('./public/',(err)=>{
+              if(err) {
+                return console.log(err);
+              }
+              else{
+                console.log("Public Directory created.");
+                let dir= './public';
+                fs.mkdir(dir + '/files/', (err)=> {
+                  if (err) {
+                    return console.error(err);
+                  } else{
+                    console.log("File Directory created.");
+                    cb(null, "public/files");
+                  }
+                });
+              }
+            });
+          } 
+          else {
+            console.log("Public Directory exists.")
+            fs.access('public/files', function(error) {
+                  if(error) {
+                    console.log("Files Directory does not exist!!");
+                    fs.mkdir('./public/files',(err)=>{
+                      if(err) {
+                        return console.log(err);
+                      }
+                      else{
+                        console.log("Files directory created.");
+                        cb(null, "public/files");
+                      }
+                    });
+                  }
+                  else{
+                    console.log("Files Directory exists!!");
+                    cb(null, "public/files");
+                  }
+              });
+          }
+        });
+      }
   },
   filename: (req, file, cb) => {
       if(file.fieldname==="image"){
