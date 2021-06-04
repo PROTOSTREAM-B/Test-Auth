05ab5764c52d144ff22cdd463c40959fa81c403f
Ujjwal-os <67314893+Ujjwal-os@users.noreply.github.com>
2021-06-04 11:53:37 +0530

    Update scheme.js

--- controllers/scheme.js
+++ controllers/scheme.js
@@ -83,31 +83,31 @@ exports.DeleteScheme = (req, res) => {
   });
 };
 
-exports.createNewScheme = (req, res) => {
-  let form = new formidable.IncomingForm();
-  form.keepExtensions = true;
-  form.parse(req, (err, fields, file) => {
-    if (err) {
-      console.log("error");
-      console.log(err);
-      return res.status(400).json({
-        error: "problem with uploads",
-      });
-    }
+// exports.createNewScheme = (req, res) => {
+//   let form = new formidable.IncomingForm();
+//   form.keepExtensions = true;
+//   form.parse(req, (err, fields, file) => {
+//     if (err) {
+//       console.log("error");
+//       console.log(err);
+//       return res.status(400).json({
+//         error: "problem with uploads",
+//       });
+//     }
 
-    let scheme = new Scheme(fields);
-    scheme.details.data = fs.readFileSync(file.details.path);
-    scheme.details.contentType = file.details.type;
-    scheme.image.data = fs.readFileSync(file.image.path);
-    scheme.image.contentType = file.image.type;
+//     let scheme = new Scheme(fields);
+//     scheme.details.data = fs.readFileSync(file.details.path);
+//     scheme.details.contentType = file.details.type;
+//     scheme.image.data = fs.readFileSync(file.image.path);
+//     scheme.image.contentType = file.image.type;
 
-    scheme.save((err, scheme) => {
-      if (err) {
-        res.status(400).json({
-          error: "saving failed",
-        });
-      }
-      res.status(200).json(scheme);
-    });
-  });
-};
+//     scheme.save((err, scheme) => {
+//       if (err) {
+//         res.status(400).json({
+//           error: "saving failed",
+//         });
+//       }
+//       res.status(200).json(scheme);
+//     });
+//   });
+// };
